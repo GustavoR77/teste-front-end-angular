67b36fde31350a74e9b666911a95397540fd4173
Gustavo Ribeiro <52572546+GustavoR77@users.noreply.github.com>
2023-04-28 18:07:19 -0300

    table 1

--- teste-angular/src/app/app.module.ts
+++ teste-angular/src/app/app.module.ts
@@ -4,12 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { CreateEditComponent } from './components/create-edit/create-edit.component';
+import { TableListComponent } from './components/table-list/table-list.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, AppRoutingModule, MatToolbarModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CreateEditComponent,
+    TableListComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
